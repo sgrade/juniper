7af754ddff8826c428760757e8a40d0769be0113
Roman Klyuev <romankmail@gmail.com>
2018-02-19 17:15:19 +0300

    First working version of PyEz-based JNCIE lab config loader

--- pythonj/pyezj/tools/yml_parser.py
+++ pythonj/pyezj/tools/yml_parser.py
@@ -4,8 +4,9 @@ import yaml
 def parse_yml(config='loader.yml'):
     """
     Creates a dictionary from config file
+
     :type config: string
-    :rtype: dict
+    :rtype: dictionary
     """
     with open(config) as yml_file:
         dictionary = yaml.load(yml_file)
